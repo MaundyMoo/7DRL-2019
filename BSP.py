1b359517f41a141dc8dde9a96b39605875c2621c
MaundyMoo <maund007@gmail.com>
2019-03-06 11:42:33 +0000

    Working on BSP

--- BSP.py
+++ BSP.py
@@ -7,6 +7,13 @@ class BinaryTree:
     def getRootNode(self):
         return self.root
 
+    def postOrderTraversal(self, root, returnList: list = []):
+        if root:
+            self.postOrderTraversal(root.getleftChild())
+            self.postOrderTraversal(root.getrightChild())
+            returnList.append(root)
+        return returnList
+
 # Need to decide what attributes the node object should have
 class Node:
     # Idk if parsing in the parent node is necessary
@@ -15,6 +22,12 @@ class Node:
         self.leftChild = leftChild
         self.rightChild = rightChild
         self.parentNode = parent
+
+    def __repr__(self):
+        _str = ""
+        for each in self.array:
+            _str += (str(each) + '\n').rstrip(' ')
+        return _str
 
     def setleftChild(self, node):
         self.leftChild = node
@@ -40,26 +53,42 @@ class Node:
     def getParent(self):
         return self.parentNode
 
+    def returnChildren(self) -> tuple:
+        return (self.leftChild, self.rightChild)
+
     def split(self):
         h, w = len(self.array)-1, len(self.array[0])-1
         r = random.randint(0, 1)
         #Split vertically
         if r == 0:
+            print('vertical')
             splitindex = random.randint(boundary_size, h - boundary_size)
             left, right = [], []
-            for y in range(len(self.array)):
-                left.append(self.array[y][:splitindex])
-                right.append(self.array[y][splitindex:])
-            print(len(self.array), len(left[0]), len(right[0]))
+            if len(self.array[0][0]) > boundary_size:
+                for y in range(len(self.array)):
+                    left.append(self.array[y][:splitindex])
+                    right.append(self.array[y][splitindex:])
+                self.setleftChild(Node(left))
+                self.setrightChild(Node(right))
         else:
-            v = random.randint(0, w)
+            print('print horizontal')
+            splitindex = random.randint(boundary_size, w - boundary_size)
+            if len(self.array[0]) > boundary_size:
+                v = random.randint(0, w)
+                top = self.array[:splitindex]
+                bottom = self.array[splitindex:]
+                self.setleftChild(Node(top))
+                self.setrightChild(Node(bottom))
 
-def generate(w: int, h: int, min_size: int = 5) -> list:
+def generate(w: int, h: int, min_size: int = 5, iterations: int = 3) -> list:
     global boundary_size
     boundary_size = min_size
     arr = [[None for i in range(w)] for j in range(h)]
     Root = Node(arr)
     Tree = BinaryTree(Root)
     Tree.getRootNode().split()
+    print(Tree.getRootNode(), '-' * 20 + '\n', Tree.getRootNode().getleftChild(), '-' * 20 + '\n', Tree.getRootNode().getrightChild(), end='', sep='')
+    print(Tree.postOrderTraversal(Tree.getRootNode()))
+
 
 generate(20, 20)
