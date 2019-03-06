e03179e4bb65179add9410109f5b4bdc5cbec201
MaundyMoo <maund007@gmail.com>
2019-03-06 13:30:08 +0000

    BSP work

--- BSP.py
+++ BSP.py
@@ -22,13 +22,13 @@ class Node:
         self.leftChild = leftChild
         self.rightChild = rightChild
         self.parentNode = parent
-
+    '''
     def __repr__(self):
         _str = ""
         for each in self.array:
             _str += (str(each) + '\n').rstrip(' ')
         return _str
-
+    '''
     def setleftChild(self, node):
         self.leftChild = node
 
@@ -60,20 +60,20 @@ class Node:
         h, w = len(self.array)-1, len(self.array[0])-1
         r = random.randint(0, 1)
         #Split vertically
-        if r == 0:
+        if r == 0 and h > 2*boundary_size:
             print('vertical')
             splitindex = random.randint(boundary_size, h - boundary_size)
             left, right = [], []
-            if len(self.array[0][0]) > boundary_size:
+            if len(self.array[0]) > boundary_size:
                 for y in range(len(self.array)):
                     left.append(self.array[y][:splitindex])
                     right.append(self.array[y][splitindex:])
                 self.setleftChild(Node(left))
                 self.setrightChild(Node(right))
-        else:
-            print('print horizontal')
+        elif w > 2*boundary_size:
+            print('horizontal')
             splitindex = random.randint(boundary_size, w - boundary_size)
-            if len(self.array[0]) > boundary_size:
+            if len(self.array) > boundary_size:
                 v = random.randint(0, w)
                 top = self.array[:splitindex]
                 bottom = self.array[splitindex:]
@@ -86,9 +86,19 @@ def generate(w: int, h: int, min_size: int = 5, iterations: int = 3) -> list:
     arr = [[None for i in range(w)] for j in range(h)]
     Root = Node(arr)
     Tree = BinaryTree(Root)
+    iterate(Tree.getRootNode(), iterations)
+    print(Tree.postOrderTraversal(Tree.getRootNode()))
+    '''
     Tree.getRootNode().split()
     print(Tree.getRootNode(), '-' * 20 + '\n', Tree.getRootNode().getleftChild(), '-' * 20 + '\n', Tree.getRootNode().getrightChild(), end='', sep='')
     print(Tree.postOrderTraversal(Tree.getRootNode()))
+    '''
 
+def iterate(node, maxiters: int, iter: int = 0):
+    if not iter == maxiters and not node is None:
+        iter += 1
+        node.split()
+        iterate(node.getleftChild(), maxiters, iter)
+        iterate(node.getrightChild(), maxiters, iter)
 
 generate(20, 20)
